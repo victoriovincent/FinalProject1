f0c73c8043f070c11360795bd4bfe80d04022e3f
victoriovincent <victorio.vincent@yahoo.com>
2021-11-30 13:53:51 +0700

    first commit

--- Sesi12/UnitTest_Mock/UnitTest_Mock/Services/EmployeeService.cs
+++ Sesi12/UnitTest_Mock/UnitTest_Mock/Services/EmployeeService.cs
@@ -1,9 +1,10 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnitTest_Mock.Models;
+
 
 namespace UnitTest_Mock.Services
 {
---
+++ Sesi12/UnitTest_Mock/UnitTesting/EmployeeTest.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest_Mock.Controllers;
+using UnitTest_Mock.Models;
+using UnitTest_Mock.Services;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class EmployeeTest
+    {
+        #region Property
+        public Mock<IEmployeeService> mock = new Mock<IEmployeeService>();
+        #endregion
+
+        [Fact]
+        public async void GetEmployeeById()
+        {
+            mock.Setup(p => p.GetEmployeebyId(1)).ReturnsAsync("JK");
+            EmployeeController emp = new EmployeeController(mock.Object);
+            string result = await emp.GetEmployeeById(1);
+            Assert.Equal("JK", result);
+        }
+
+        [Fact]
+        public async void GetEmployeeDetails()
+        {
+            var employeeDTO = new Employee()
+            {
+                id = 1,
+                name = "JK",
+                desgination = "SDE"
+            };
+            mock.Setup(p => p.GetEmployeeDetails(1)).ReturnsAsync(employeeDTO);
+            EmployeeController emp = new EmployeeController(mock.Object);
+            var result = await emp.GetEmployeeDetails(1);
+            Assert.True(employeeDTO.Equals(result));
+        }
+    }
+}
